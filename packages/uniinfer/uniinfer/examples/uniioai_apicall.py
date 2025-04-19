d40902bd221cfbd7d488479a4cfbc431f46895be
devskale <dev@skale.io>
2025-04-19 11:28:08 +0200

    yay, maxtokens updated

--- packages/uniinfer/uniinfer/examples/uniioai_apicall.py
+++ packages/uniinfer/uniinfer/examples/uniioai_apicall.py
@@ -1,4 +1,3 @@
-from credgoo import get_api_key
 from openai import OpenAI
 
 provider = 'arli'
@@ -7,12 +6,18 @@ model = 'Mistral-Nemo-12B-Instruct-2407'
 provider = 'groq'
 model = 'qwen-qwq-32b'
 
-provider = 'ollama'
-model = 'gemma3:4b'
+# provider = 'ollama'
+# model = 'gemma3:4b'
+# model = 'phi4-mini:latest'
 
+# the baseurl where uniinfer oai proxy server is running
 baseurl = 'http://localhost:8000/v1'
 
+# IMPORTANT
+# the key is required for underlying uniinfer package to retrieve provider specific apikeys
+# see credgoo package for more details
 api_key = "test23@test34"
+# api_key = None
 
 client = OpenAI(
     base_url=baseurl,
@@ -22,7 +27,7 @@ client = OpenAI(
 # Get user input dynamically
 user_message = input("Please enter your message (ENTER for default): ").strip()
 if not user_message:
-    user_message = "Berlin, Sao Paulo or Mumbai. choose only one. explain your decision briefly in the style: CITY: Because: EXPLANATIONSENTENCE"
+    user_message = "Berlin, Sao Paulo or Mumbai? Out of these choose a winner! Explain your winner decision briefly in the style: WINNERCITY: Because: EXPLANATIONSENTENCE"
 print('\n\n--')
 print(f"{provider}@{model}")
 print(f"User message: {user_message}")
@@ -31,7 +36,8 @@ completion = client.chat.completions.create(
     model=f"{provider}@{model}",  # Specify the model here
     messages=[{"role": "user",
                "content": user_message}],
-    stream=True  # Enable streaming
+    stream=True,  # Enable streaming
+    max_tokens=1024,
 )
 
 
