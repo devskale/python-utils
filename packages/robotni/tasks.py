17d5376e6dd82537bbcb4be0fd278df26991963e
devskale <dev@skale.io>
2025-06-14 11:14:03 +0200

    up

---
+++ packages/robotni/tasks.py
@@ -0,0 +1,10 @@
+from huey_app import huey
+from workers.taskFakejob import taskFakejob as original_taskFakejob
+
+@huey.task()
+def add(x, y):
+    return x + y
+
+@huey.task()
+def run_fake_job():
+    return original_taskFakejob()
