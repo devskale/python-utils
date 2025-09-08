828d691b87a6dd026085717cb9ed7c959c6dbd35
skale <dev@skale.dev>
2025-09-08 10:12:21 +0200

    token count added

--- packages/uniinfer/setup.py
+++ packages/uniinfer/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="uniinfer",
-    version="0.1.8",
+    version="0.1.9",
     url="https://github.com/skale-dev/uniinfer",
     description="Unified Inference API for LLM chat completions",
     author="Han Woo",
